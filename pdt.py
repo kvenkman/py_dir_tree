49280fe4d1db884b1d643846fa8a50bd6ee8ce31
kvenkman <karthikvt@vt.edu>
2022-08-07 14:59:51 -0700

    update to make callable within python

--- pdt.py
+++ pdt.py
@@ -35,23 +35,10 @@ def return_current_structure(this_path, current_depth, max_depth, filter_hidden=
         
     return subtree_struct
 
-def main():
-    parser = argparse.ArgumentParser(description='Print directory structure for requested folder or pipe it to a folder')
-
-    parser.add_argument('path')
-    parser.add_argument('-p', '--path', type=str, nargs=1, default = '.', help='Directory tree will begin at this path (default: current directory')
-    parser.add_argument('-o', '--output-file', type=str, nargs=1, default=None, help='If specified, directory tree will be written to this file')
-    parser.add_argument('-d', '--depth', type=int, nargs=1, default=[5], help='Maximum depth to recurse (default: 5). Set to -1 to recurse all the way to the bottom')
-    parser.add_argument('-f', '--filter-hidden', action='store_true', help='Filter hidden files and folders (default: False)')
-    args = parser.parse_args()
-
-    output_path = args.output_file
-    max_depth = args.depth[0]
-    path = Path(args.path)
-    filter_hidden = args.filter_hidden
+def create_dir_tree(dirpath=Path('.'), output_path=None, max_depth=5, filter_hidden=False):
 
     current_depth = 0
-    tree_structure = return_current_structure(path, current_depth+1, max_depth, filter_hidden)
+    tree_structure = return_current_structure(dirpath, current_depth+1, max_depth, filter_hidden)
     
     if output_path is not None:
         with open(output_path[0], 'w') as outf:
@@ -60,4 +47,18 @@ def main():
         print(tree_structure)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Print directory structure for requested folder or pipe it to a folder')
+
+    parser.add_argument('path')
+    parser.add_argument('-p', '--path', type=str, nargs=1, default = '.', help='Directory tree will begin at this path (default: current directory')
+    parser.add_argument('-o', '--output-file', type=str, nargs=1, default=[None], help='If specified, directory tree will be written to this file')
+    parser.add_argument('-d', '--depth', type=int, nargs=1, default=[5], help='Maximum depth to recurse (default: 5). Set to -1 to recurse all the way to the bottom')
+    parser.add_argument('-f', '--filter-hidden', action='store_true', help='Filter hidden files and folders (default: False)')
+    args = parser.parse_args()
+
+    dirpath = Path(args.path)
+    output_path = args.output_file[0]
+    max_depth = args.depth[0]
+    filter_hidden = args.filter_hidden
+
+    create_dir_tree(dirpath, output_path, max_depth, filter_hidden)
