f4b2ef5ca896829b48085e875250f0086077fe07
kvenkman <karthikvt@vt.edu>
2022-08-07 15:23:13 -0700

    make package installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pdt',
+    version='0.1.0',    
+    description='A Python package that generates a directory tree',
+    url='https://github.com/kvenkman/py_dir_tree',
+    author='Karthik Venkataramani',
+    license='MIT License',
+    packages=['pdt'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: General',
+        'License :: MIT',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
